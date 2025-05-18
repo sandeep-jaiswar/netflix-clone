4ecac4b163a8a0a8117962aa43f03ffb68b35c77
jaiswarsandeep119 <jaiswarsandeep119@gmail.com>
2025-05-18 17:11:30 +0000

    chore: add jest, pg and prisma

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  transform: {
+    '^.+\.(ts|tsx)$': 'babel-jest',
+  },
+};
